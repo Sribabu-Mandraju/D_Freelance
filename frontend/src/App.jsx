9350f31f66ae1aa14d8112fe1d3ea78083d9f4cd
sireeshareddy <sibbalasireeshareddy31@gmail.com>
2025-08-04 19:25:34 +0530

    ghj

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './Pages/Home';
-const App = () => {
-  return (
-      <Router>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-        </Routes>
+// import React from 'react'
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import Home from './Pages/Home';
+// import cloudinary from './Components/main/cloudinary';
+
+// const App = () => {
+//   return (
+//       <Router>
+//         <Routes>
+//             <Route path="/" element={<Home/>}/>
+//             <Route path="/clodinary" element={<cloudinary/>}/>
+//         </Routes>
             
-      </Router>
+//       </Router>
+//   )
+// }
+
+// export default App
+
+
+
+import React from "react"
+import Cloudinary from "./Components/main/Cloudinary"
+const App=()=>{
+  return(
+<div>
+  <Cloudinary/>
+</div>
   )
 }
-
 export default App
