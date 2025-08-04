9b6471dc5d0141173daa078d44293d2a0b7a1684
sireeshareddy <sibbalasireeshareddy31@gmail.com>
2025-08-04 19:26:31 +0530

    tyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,28 @@
-// import React from 'react'
-// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-// import Home from './Pages/Home';
-// import cloudinary from './Components/main/cloudinary';
-
-// const App = () => {
-//   return (
-//       <Router>
-//         <Routes>
-//             <Route path="/" element={<Home/>}/>
-//             <Route path="/clodinary" element={<cloudinary/>}/>
-//         </Routes>
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Pages/Home';
+const App = () => {
+  return (
+      <Router>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+        </Routes>
             
-//       </Router>
-//   )
-// }
-
-// export default App
-
-
-
-import React from "react"
-import Cloudinary from "./Components/main/Cloudinary"
-const App=()=>{
-  return(
-<div>
-  <Cloudinary/>
-</div>
+      </Router>
   )
 }
+
 export default App
+
+
+
+// import React from "react"
+// import Cloudinary from "./Components/main/Cloudinary"
+// const App=()=>{
+//   return(
+// <div>
+//   <Cloudinary/>
+// </div>
+//   )
+// }
+// export default App
