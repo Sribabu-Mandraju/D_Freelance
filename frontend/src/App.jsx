48370a83214b27ddc3aa70b16f206c8595faac33
BinduSree-18 <palukurthibindu18@gmail.com>
2025-08-05 06:18:50 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Pages/Home';
+import Footer from './Pages/Footer';
 const App = () => {
   return (
       <Router>
         <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/footer" element={<Footer/>}/> 
         </Routes>
             
       </Router>
