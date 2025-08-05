2b4a3a06e0089d4eaaa0e76a70f998276a88a389
sireeshareddy <sibbalasireeshareddy31@gmail.com>
2025-08-06 02:52:56 +0530

    yui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Pages/Home';
 import Portfolio from './Pages/Portfolio';
+import VulgarTest from './Components/vulgarTest';
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/portfolio" element={<Portfolio/>}/>
-           
+            <Route path="/VulgarTest" element={<VulgarTest/>}/> 
             {/* Add more routes as needed */}
          
         </Routes>
